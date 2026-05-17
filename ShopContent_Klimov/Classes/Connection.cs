19a16fb96ff8d0b220b41f6c627503c04469fb54
STEPS0N <stepan.klimoff@mail.ru>
2026-05-17 18:16:23 +0500

    Разработанные классы общего пользования. Климов

---
+++ ShopContent_Klimov/Classes/Connection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopContent_Klimov.Classes
+{
+    public class Connection
+    {
+        private static readonly string config = "server=localhost;" +
+            "Trusted_Connection=No;" +
+            "DataBase=ShopContent;" +
+            "User=root;" +
+            "PWD=;";
+
+        public static SqlConnection OpenConnection()
+        {
+            SqlConnection connection = new SqlConnection(config);
+            connection.Open();
+            return connection;
+        }
+
+        public static SqlDataReader Query(string SQL, out SqlConnection connection)
+        {
+            connection = OpenConnection();
+            return new SqlCommand(SQL, connection).ExecuteReader();
+        }
+
+        public static void CloseConnection(SqlConnection connection)
+        {
+            connection.Close();
+            SqlConnection.ClearPool(connection);
+        }
+    }
+}
