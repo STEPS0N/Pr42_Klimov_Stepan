5ca579245367517a755f1e73f4db357daa7af65c
STEPS0N <stepan.klimoff@mail.ru>
2026-05-18 20:58:47 +0500

    Разработанные модели представления. Климов

---
+++ ShopContent_Klimov/ViewModel/VMCategorys.cs
@@ -0,0 +1,32 @@
+﻿using ShopContent_Klimov.Context;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopContent_Klimov.ViewModel
+{
+    public class VMCategorys : INotifyPropertyChanged
+    {
+        public ObservableCollection<CategorysContext> Categorys { get; set; }
+
+        public VMCategorys()
+        {
+            Categorys = CategorysContext.AllCategorys();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
---
+++ ShopContent_Klimov/ViewModel/VMItems.cs
@@ -0,0 +1,46 @@
+﻿using ShopContent_Klimov.Classes;
+using ShopContent_Klimov.Context;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopContent_Klimov.ViewModel
+{
+    public class VMItems : INotifyPropertyChanged
+    {
+        public ObservableCollection<ItemsContext> Items { get; set; }
+
+        public RelayCommand NewItem
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                {
+                    ItemsContext newModel = new ItemsContext(true);
+                    Items.Add(newModel);
+                    MainWindow.init.frame.Navigate(new View.Add(newModel));
+                });
+            }
+        }
+
+        public VMItems()
+        {
+            Items = ItemsContext.AllItems();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
